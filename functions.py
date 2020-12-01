f9d0dad974cda16b080eaf0423817d0b417abc67
Francois Vieux <francois.g.vieux@gmail.com>
2020-12-01 16:37:07 +0100

    done

--- functions.py
+++ functions.py
@@ -1,13 +1,24 @@
 # -*-coding:utf-8 -*
-from data import words, bestScores
 from random import randint
+import pickle
+
+
+
+words = ['hello']
+bestScores = {}
+
+with open('data.py', 'rb') as datas:
+     monDepickler = pickle.Unpickler(datas)
+     bestScores = monDepickler.load()
+
+#, 'bonjour', 'hallo', 'gutentag', 'hi', 'bonsoir'
 
 def getPlayerScore(player): 
     if player in bestScores:
         print('Your best score yet: ' + str(bestScores[player]))
     else:
         bestScores[player] = 0 
-        print(bestScores)
+        print('Happy first game!!')
 
 def selectWordToGuess():
     i = randint(0, len(words)-1)
@@ -34,10 +45,17 @@ def getLetter(word, hiddenWord):
                 secret += '*'
         return 'True', letter, secret
     return 'False', letter, hiddenWord
+
+def playAgain():
+    motiv = input('Type 0 to play again :')
+    if motiv == 'O' or motiv == 'o' or motiv == '0':
+        return True 
     
-# def testWord(word):
 
-# def incrementPlayerScore(newScore): 
-
-# def showBestScores(): 
+def savePlayerScore(newScore, currentPlayer): 
+    if newScore > bestScores[currentPlayer]: 
+        bestScores[currentPlayer] = newScore
+        with open('data.py', 'wb') as datas:
+            mon_pickler = pickle.Pickler(datas)
+            mon_pickler.dump(bestScores)
 
--- pendu.py
+++ pendu.py
@@ -4,17 +4,20 @@ from functions import *
 os.chdir('../pendu_pv')
 
 #variables declaration
+continuPlaying = True
+currentScore = 0
 guesses = 8
-#player = input('Input your name here :')
-player = 'plop' # while developing the game set the player name here
+player = input('Input your name here :')
+#player = 'plop' # while developing the game set the player name here
 word = selectWordToGuess()
 hiddenWord = createHiddenWord(word)
 guessedLetter = []
 
 # game starts here
 getPlayerScore(player)
+print('Secret word: ' + hiddenWord)
 
-while guesses > 0:
+while guesses > 0 and continuPlaying == True:
         currentResult, currentLetter, updatedHiddenWord = getLetter(word, hiddenWord)
         try:
             if currentResult == 'False': 
@@ -30,11 +33,22 @@ while guesses > 0:
 
             if hiddenWord == word:
                 print('you won!!')
-                break
+                currentScore += guesses
+                continuPlaying = playAgain()
+                if continuPlaying == True:
+                    guesses = 8
+                    word = selectWordToGuess()
+                    hiddenWord = createHiddenWord(word)
+                    guessedLetter = []
+                else:
+                    break
                 
         finally:
             print('Secret word: ' + hiddenWord)
             print( 'You tried the letters : ' + ', '.join(guessedLetter))
-            print('Guesses left: ' + str(guesses))
+
+print('thank you for playing')
+savePlayerScore(currentScore, player)
+             
 
 
